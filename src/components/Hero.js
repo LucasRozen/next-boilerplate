7e2c7886edceeaeaa77791a7f40ec5847ad20834
lukirozen <lucaslrozenberg@gmail.com>
2023-09-27 19:29:52 -0300

    cambios2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div
-      className={`hero flex justify-center items-center w-full h-[600px] bg-[url('/assets/12monitos.jpg')] bg-cover bg-top mb-12`}
+      className={`hero flex justify-center items-center w-full h-[600px] bg-black-gradient-radial bg-black bg-cover bg-top mb-12`}
     >
       <div className=' bg-[rgba(0,0,0,0.6)] w-full h-full flex justify-center items-center'>
         <h1 className=' text-7xl text-yellow-300'>Batman Shows Website</h1>
